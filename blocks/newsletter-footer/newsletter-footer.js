af2dc302616d7b57e750654be49867e8e5b051a5
William Ste-Marie <williambsm@hotmail.fr>
2022-03-21 16:08:49 -0400

    feat(newsletter-footer): close button

--- blocks/newsletter-footer/newsletter-footer.js
+++ blocks/newsletter-footer/newsletter-footer.js
@@ -5,5 +5,23 @@ import {
 import createTag from '../gnav/gnav-utils.js';
 
 export default async function decorate($block) {
+    const $links = $block.querySelectorAll('a');
+
+    $links.forEach(($link) => {
+        if ($link.href.match('newsletter-modal')) {
+            $link.classList.add('newsletter-footer-cta');
+        }
+    });
+
+    const $close = createTag('a', { class: 'newsletter-footer-close' });
+    const $closeIcon = createTag('img', { src: '/blocks/newsletter-footer/close.svg' });
+
+    $close.addEventListener('click', (e) => {
+        $block.remove();
+    });
+
+    $close.append($closeIcon);
+    $block.append($close);
+
     return $block;
 }

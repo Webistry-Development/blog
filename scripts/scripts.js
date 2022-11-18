580f72719f3cd5f85cfc963a39c971b6a3825d87
William Ste-Marie <williambsm@hotmail.fr>
2022-11-17 20:34:39 -0500

    Revert "fix(topics): Breaking code and compatibility issues"
    This reverts commit ddbdcbc9e9a7627c4fab8a9304835912dde9d961.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -702,19 +702,13 @@ function buildArticleHeader(mainEl) {
 
 function buildTagHeader(mainEl) {
   const div = mainEl.querySelector('div');
-
-  if (div) {
-    const heading = div.querySelector('h1, h2');
-    const picture = div.querySelector('picture');
-
-    if (picture) {
-      const tagHeaderBlockEl = buildBlock('tag-header', [
-        [heading],
-        [{ elems: [picture.closest('p')] }],
-      ]);
-      div.prepend(tagHeaderBlockEl);
-    }
-  }
+  const heading = mainEl.querySelector('h1, h2');
+  const picture = mainEl.querySelector('picture');
+  const tagHeaderBlockEl = buildBlock('tag-header', [
+    [heading],
+    [{ elems: [picture.closest('p')] }],
+  ]);
+  div.prepend(tagHeaderBlockEl);
 }
 
 function buildAuthorHeader(mainEl) {
